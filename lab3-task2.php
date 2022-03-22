49b88e3949304d539dc8d1a8a5f9a617dc6d058c
eanghengty <hengty174@gmail.com>
2022-03-22 16:10:04 +0700

    done

--- lab3-task2.php
+++ lab3-task2.php
@@ -1,9 +1,10 @@
 <?php
 //filter out the even number from the array
 $arr = [2,3,4,6,7,9,11,20];
-$filter_out_even_number = array_map(fn($idx)=> $idx %2 !== 0 ? $arr2= $idx:null ,$arr);
+$filter_out_even_number =fn($idx)=> ($idx % 2 !==0 ? true : false);
 
-print_r($filter_out_even_number);
+
+print_r(array_filter($arr,$filter_out_even_number));
 
 
 
